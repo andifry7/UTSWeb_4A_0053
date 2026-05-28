ed63dc62b1a83db699e33b6aba3e11c55b914f55
Andifry <andifry.github7@gmail.com>
2026-05-28 22:02:35 +0700

    feat: menambahkan efek active navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Pixel++ Website Loaded");
+
+// Navbar Scroll Effect
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+    $(".navbar").addClass("navbar-scrolled");
+  } else {
+    $(".navbar").removeClass("navbar-scrolled");
+  }
+});
